5af82d09101b8757213e6e94763b26e16162fa4b
Ivo van Poorten <ivopvp@gmail.com>
2024-04-16 00:01:56 +0200

    Add Lorenz test for SLO and test implementation. Pass!

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 #include "fake6502.h"
 
 uint8_t memory[65536];
@@ -12,8 +14,8 @@ void write6502(uint16_t address, uint8_t value) {
     memory[address] = value;
 }
 
-void test(const char *filename, uint16_t success) {
-    printf("Test: %s -- ", filename);
+void test(const char *filename, uint16_t success, bool trace) {
+    printf("%s -- ", filename);
     FILE *f = fopen(filename,"rb");
     if (!f) {
         fprintf(stderr, "cannot open test\n");
@@ -23,29 +25,48 @@ void test(const char *filename, uint16_t success) {
         fprintf(stderr, "premature EOF\n");
         return;
     }
+    fflush(stdout);
     reset6502();
     while (1) {
         uint16_t save = PC;
         step6502();
+        if (trace) {
+            printf("PC=%04x ", PC);
+            printf("A=%02x X=%02x Y=%02x SP=%02x P=%02x\n", A, X, Y, SP, getP());
+        }
         if (save == PC) {
-            printf("%s ", PC != success ? "FAIL!" : "PASS");
-            printf("PC = $%04x, instructions = %ld\n", PC, instructions);
-            printf("\tA=%02x X=%02x Y=%02x SP=%02x P=%02x\n", A, X, Y, SP, getP());
+            printf("%s --- ", PC != success ? "FAIL!" : "pass");
+            printf("PC=%04x ", PC);
+            printf("A=%02x X=%02x Y=%02x SP=%02x P=%02x\n", A, X, Y, SP, getP());
             return;
         }
     }
 }
 
 void main(void) {
-    test("test/6502_functional_test.bin", 0x3469);
+    test("test/6502_functional_test.bin", 0x3469, false);
     instructions = 0;
-    test("test/6502_decimal_test.bin", 0x044b);
+    test("test/6502_decimal_test.bin", 0x044b, false);
     instructions = 0;
-    test("test/bird6502.bin", 0x861c);
+    test("test/bird6502.bin", 0x861c, false);
     instructions = 0;
-    test("test/ttl6502.bin", 0xf5ea);
+    test("test/ttl6502.bin", 0xf5ea, false);
     instructions = 0;
-    test("test/6502DecimalMode.bin", 0x8133);
+    test("test/6502DecimalMode.bin", 0x8133, false);
     instructions = 0;
-    test("test/cpu_decimal.bin", 0x302f);
+    test("test/cpu_decimal.bin", 0x302f, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoa.bin", 0x08b3, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoax.bin", 0x08ca, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoay.bin", 0x08ca, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoix.bin", 0x08c4, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoiy.bin", 0x08ce, false);
+    instructions = 0;
+    test("test/lorenz/slo_asoz.bin", 0x08b6, false);
+    instructions = 0;
+    test("test/lorenz/slo_asozx.bin", 0x08c0, false);
 }
---
+++ test/lorenz/slo_asozx.asm
@@ -0,0 +1,177 @@
+
+.ifndef ATARI
+    opt h-
+    opt f+
+    org 0
+    dta 0
+    .align $0801,0
+.else
+    org $0801
+.endif
+
+main
+         jsr print
+         .byte 13
+         .byte "(up)asozx"
+         .byte 0
+
+         lda #%00011011
+         sta db
+         lda #%11000110
+         sta ab
+         lda #%10110001
+         sta xb
+         lda #%01101100
+         sta yb
+         lda #0
+         sta pb
+         tsx
+         stx sb
+
+         lda #0
+         sta db
+         sta xb
+
+next     lda db
+         sta 172
+         asl
+         sta dr
+
+         ora ab
+         sta ar
+
+         lda xb
+         sta xr
+
+         lda yb
+         sta yr
+
+         lda pb
+         ora #%00110000
+         and #%01111100
+         tax
+         lda ar
+         cmp #0
+         bne nozero
+         txa
+         ora #%00000010
+         tax
+         lda ar
+nozero   asl
+         bcc noneg
+         txa
+         ora #%10000000
+         tax
+noneg    lda db
+         bpl nocarry
+         txa
+         ora #%00000001
+         tax
+nocarry  stx pr
+
+         lda sb
+         sta sr
+
+         ldx sb
+         txs
+         lda pb
+         pha
+         lda ab
+         ldx xb
+         ldy yb
+         plp
+
+cmd      .byte $17
+         .byte 172
+
+         php
+         cld
+         sta aa
+         stx xa
+         sty ya
+         pla
+         sta pa
+         tsx
+         stx sa
+         lda 172
+         sta da
+         jsr check
+
+
+         inc xb
+         dec cmd+1
+
+         inc db
+         bne jmpnext
+         inc pb
+         beq nonext
+jmpnext  jmp next
+nonext
+
+success jmp *
+
+db       .byte 0
+ab       .byte 0
+xb       .byte 0
+yb       .byte 0
+pb       .byte 0
+sb       .byte 0
+da       .byte 0
+aa       .byte 0
+xa       .byte 0
+ya       .byte 0
+pa       .byte 0
+sa       .byte 0
+dr       .byte 0
+ar       .byte 0
+xr       .byte 0
+yr       .byte 0
+pr       .byte 0
+sr       .byte 0
+
+check
+         lda da
+         cmp dr
+         bne error
+         lda aa
+         cmp ar
+         bne error
+         lda xa
+         cmp xr
+         bne error
+         lda ya
+         cmp yr
+         bne error
+         lda pa
+         cmp pr
+         bne error
+         lda sa
+         cmp sr
+         bne error
+         rts
+
+error    jmp *
+
+print    pla
+         sta print0+1
+         pla
+         sta print0+2
+         ldx #1
+print0   lda *,x
+         beq print1
+         ;jsr $ffd2
+         inx
+         bne print0
+print1   sec
+         txa
+         adc print0+1
+         sta print2+1
+         lda #0
+         adc print0+2
+         sta print2+2
+print2   jmp *
+
+.ifndef ATARI
+    org $fffa
+    dta a(0),a(main),a(0)
+.endif
